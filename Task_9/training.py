f0b8caf4dc76bc9a5b3b27453fe8e80dadb737c7
balage <balage@EndeavourOS.home>
2025-01-18 15:31:10 +0100

    new hope

--- Task_9/training.py
+++ Task_9/training.py
@@ -6,8 +6,8 @@ from stable_baselines3.common.callbacks import BaseCallback, EvalCallback
 from env_V3_final_2 import CustomEnv  # Assuming CustomEnv is in this file
 from stable_baselines3.common.monitor import Monitor
 from clearml import Task
-import typing_extensions 
 import tensorboard
+from datetime import datetime
 
 # Initialize command-line argument parser for hyperparameters
 parser = argparse.ArgumentParser()
@@ -17,10 +17,12 @@ parser.add_argument("--n_steps", type=int, default=1000)  # 1000 steps per itera
 parser.add_argument("--n_epochs", type=int, default=10)
 parser.add_argument("--episodes", type=int, default=10)  # Total number of episodes
 parser.add_argument("--iterations_per_episode", type=int, default=20000)  # Iterations per episode
+parser.add_argument("--gamma", type=float, default=0.98)  # Discount factor
+parser.add_argument("--ent_coef", type=float, default=0.02)  # Entropy coefficient for exploration
 args = parser.parse_args()
 
 # Set WandB API key and initialize the project
-os.environ['WANDB_API_KEY'] = '8afbb298b3eae0f6035d2e3b3bdcadf08ebb1a41'  # Use your actual API key here
+wandb.login()  # This will prompt for a login if needed, using the credentials stored
 wandb.init(project="sb3_custom_env", sync_tensorboard=True)
 
 # Set up the environment
@@ -37,6 +39,8 @@ model = PPO('MlpPolicy', env, verbose=1,
             batch_size=args.batch_size, 
             n_steps=args.n_steps, 
             n_epochs=args.n_epochs, 
+            gamma=args.gamma,  # Use gamma from command-line args
+            ent_coef=args.ent_coef,  # Use entropy coefficient from command-line args
             tensorboard_log=f"./ppo_custom_env_tensorboard/{wandb.run.id}/")
 
 # Training loop for a specific number of episodes
@@ -49,11 +53,14 @@ for episode in range(1, args.episodes + 1):
     # Train the model for the calculated number of timesteps
     model.learn(total_timesteps=total_timesteps, reset_num_timesteps=False, tb_log_name=f"PPO_run_{wandb.run.id}_episode_{episode}")
 
+    # Generate a timestamp for unique model file naming
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+
     # Save the model incrementally after each episode
-    model.save(f"ppo_model_episode_{episode}")
+    model.save(f"ppo_model_episode_{episode}_{timestamp}")
     
     # Log the model checkpoint to WandB
-    wandb.save(f"ppo_model_episode_{episode}.zip")
+    wandb.save(f"ppo_model_episode_{episode}_{timestamp}.zip")
 
 # Finish WandB logging after training is complete
 wandb.finish()
