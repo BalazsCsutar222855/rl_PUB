0856b5928d1ae718cfe2c2b71091c223759360af
balage <balage@EndeavourOS.home>
2025-01-20 20:18:35 +0100

    reward update

--- Task_9/env_V3_final_2.py
+++ Task_9/env_V3_final_2.py
@@ -62,7 +62,7 @@ class CustomEnv(gym.Env):
         action = np.append(np.array(action, dtype=np.float32), 0)
 
         # Call the environment step function
-        observation = self.sim.run([action]) # Why do we need to pass the action as a list? Think about the simulation class.
+        observation = self.simulation.run([action]) # Why do we need to pass the action as a list? Think about the simulation class.
         # now we need to process the observation and extract the relevant information, the pipette position, convert it to a numpy array, and append the goal position and make sure the array is of type np.float32
         pipette_position = np.array(observation[f'robotId_{self.sim.robotIds[0]}']['pipette_position'], dtype=np.float32)
 
